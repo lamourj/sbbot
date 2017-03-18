7c2d3e88fa110418e1bfc3096b0eca8c2caeb2b8
vwvw <nicolasbadoux@Nicolass-MacBook-Pro.local>
2017-03-18 13:33:41 +0100

    Example working across files

--- newTravel.py
+++ newTravel.py
@@ -1,3 +1,111 @@
-def newTravel(bot, update):
-    bot.sendMessage(chat_id=update.message.chat_id,
-            text="Testing");
+
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+
+import logging
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+
+
+
+GENDER, PHOTO, LOCATION, BIO = range(4)
+
+def start(bot, update):
+    reply_keyboard = [['Boy', 'Girl', 'Other']]
+
+    update.message.reply_text(
+        'Hi! My name is Professor Bot. I will hold a conversation with you. '
+        'Send /cancel to stop talking to me.\n\n'
+        'Are you a boy or a girl?',
+        reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return GENDER
+
+
+def cancel(bot, update):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation." % user.first_name)
+    update.message.reply_text('Bye! I hope we can talk again some day.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
+
+def gender(bot, update):
+    user = update.message.from_user
+    logger.info("Gender of %s: %s" % (user.first_name, update.message.text))
+    update.message.reply_text('I see! Please send me a photo of yourself, '
+                              'so I know what you look like, or send /skip if you don\'t want to.',
+                              reply_markup=ReplyKeyboardRemove())
+
+    return PHOTO
+
+
+def photo(bot, update):
+    user = update.message.from_user
+    photo_file = bot.getFile(update.message.photo[-1].file_id)
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s" % (user.first_name, 'user_photo.jpg'))
+    update.message.reply_text('Gorgeous! Now, send me your location please, '
+                              'or send /skip if you don\'t want to.')
+
+    return LOCATION
+
+
+def skip_photo(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a photo." % user.first_name)
+    update.message.reply_text('I bet you look great! Now, send me your location please, '
+                              'or send /skip.')
+
+    return LOCATION
+
+
+def location(bot, update):
+    user = update.message.from_user
+    user_location = update.message.location
+    logger.info("Location of %s: %f / %f"
+                % (user.first_name, user_location.latitude, user_location.longitude))
+    update.message.reply_text('Maybe I can visit you sometime! '
+                              'At last, tell me something about yourself.')
+
+    return BIO
+
+
+def skip_location(bot, update):
+    user = update.message.from_user
+    logger.info("User %s did not send a location." % user.first_name)
+    update.message.reply_text('You seem a bit paranoid! '
+                              'At last, tell me something about yourself.')
+
+    return BIO
+
+
+def bio(bot, update):
+    user = update.message.from_user
+    logger.info("Bio of %s: %s" % (user.first_name, update.message.text))
+    update.message.reply_text('Thank you! I hope we can talk again some day.')
+
+    return ConversationHandler.END
+
+
+ENTRY_POINTS = [CommandHandler('start', start)]
+
+STATES={
+    GENDER: [RegexHandler('^(Boy|Girl|Other)$', gender)],
+
+    PHOTO: [MessageHandler(Filters.photo, photo),
+            CommandHandler('skip', skip_photo)],
+
+    LOCATION: [MessageHandler(Filters.location, location),
+               CommandHandler('skip', skip_location)],
+
+    BIO: [MessageHandler(Filters.text, bio)]
+}
+
+FALLBACKS=[CommandHandler('cancel', cancel)]
--- sbbCffBot.py
+++ sbbCffBot.py
@@ -1,27 +1,67 @@
-from telegram.ext import Updater
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Simple Bot to reply to Telegram messages
+# This program is dedicated to the public domain under the CC0 license.
+"""
+This Bot uses the Updater class to handle the bot.
+First, a few callback functions are defined. Then, those functions are passed to
+the Dispatcher and registered at their respective places.
+Then, the bot is started and runs until we press Ctrl-C on the command line.
+Usage:
+Example of a bot-user conversation using ConversationHandler.
+Send /start to initiate the conversation.
+Press Ctrl-C on the command line or send a signal to the process to stop the
+bot.
+"""
+
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters, RegexHandler,
+                          ConversationHandler)
+import newTravel as NT
 
 
-with open("Telegram_API_token.txt", 'r') as Telegram_API_file:
+
+
+
+def error(bot, update, error):
+    logger.warn('Update "%s" caused error "%s"' % (update, error))
+
+
+def main():
+
+    with open("Telegram_API_token.txt", 'r') as Telegram_API_file:
         for line in Telegram_API_file:
             if line.startswith("token="):
                 TOKEN = line[len("token="):]
 
-updater = Updater(token=TOKEN)
+    # Create the EventHandler and pass it your bot's token.
+    updater = Updater(TOKEN)
 
-dispatcher = updater.dispatcher
+    dp = updater.dispatcher
 
-import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+    # Add conversation handler with the states GENDER, PHOTO, LOCATION and BIO
+    conv_handler = ConversationHandler(
+        entry_points=NT.ENTRY_POINTS,
+
+        states= NT.STATES,
+
+        fallbacks=NT.FALLBACKS
+    )
+
+    dp.add_handler(conv_handler)
+
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    # Run the bot until you press Ctrl-C or the process receives SIGINT,
+    # SIGTERM or SIGABRT. This should be used most of the time, since
+    # start_polling() is non-blocking and will stop the bot gracefully.
+    updater.idle()
 
 
-from telegram.ext import CommandHandler
-from start import start
-from newTravel import newTravel
-
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
-
-newTravel_handler = CommandHandler('newTravel', newTravel);
-dispatcher.add_handler(newTravel_handler);
-
-updater.start_polling()
+if __name__ == '__main__':
+    main()
